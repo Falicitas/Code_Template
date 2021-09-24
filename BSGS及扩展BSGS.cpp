f8dbf9b8f91670efc58e35131102225ad41d20ef
Kinesis <1019386658@qq.com>
2021-09-25 01:15:49 +0800

    修改了原根和bsgs的模板

--- BSGS及扩展BSGS.cpp
+++ BSGS及扩展BSGS.cpp
@@ -1,238 +1,172 @@
-int bsgs(int a,int b,int p)//应该记住的版本
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long i64;
+typedef long long ll;
+constexpr int mod = 1e9 + 7;
+
+template <class T>
+T power(T a, int b, int p = mod) {
+    T res = 1;
+    for (; b; b /= 2, a *= a, a %= p) {
+        if (b % 2) {
+            res *= a;
+            res %= p;
+        }
+    }
+    return res;
+}
+
+/*
+用于解一元同余高次方程a^x = b(\mod p)，且gcd(a,p) = 1
+的最小非负整数解。无解输出-1 。gcd(a,p)!=1，由于推导式子不等价，会得出错误结果
+*/
+int bsgs(int a, int b, int p)  //应该记住的版本
 {
-    unordered_map<int,int> m;
-    int sq = ceil(sqrt(p)),base = 1;
-    REP(i,1,sq){
-        base = 1LL * base * a % p;
-        m[1LL * b * base % p] = i;
+    unordered_map<int, int> m;
+    int sq = ceil(sqrt(p));
+    i64 base = 1;
+    for (int i = 1; i <= sq; i++) {
+        base = base * a % p;
+        m[b * base % p] = i;
     }
     int res = 1;
-    REP(i,1,sq){
-        res = 1LL * res * base % p;
-        if(m.count(res)) return i * sq - m[res];
+    for (int i = 1; i <= sq; i++) {
+        res = res * base % p;
+        if (m.count(res)) {
+            return i * sq - m[res];
+        }
     }
     return -1;
 }
 
-
-namespace HASH
-{
-    const int NHASH = 100003;
-    int head[NHASH],eid;
-    struct node
+//求x^a = b (%p),p是素数的公式。用时将其放入 BSGS 中
+int generator(int p) {
     {
-        int val,B,next;
-    }N[NHASH];//注意存储空间
-    void init()
-    {
-        memset(head,-1,sizeof head);
-        eid = 0;
-    }
-    int HASH_num(int num)
-    {
-        return (num % NHASH + NHASH) % NHASH;
-    }
-    void HASH_insert(int val,int B)//注意下面搜到存过的值
-    {
-        int num = HASH_num(val);
-        for(int i=head[num];i+1;i=N[i].next){
-            if(N[i].val==val){//相同的B取最大，保证之后第一个找到的是最小的
-                N[i].B = B;
-                return ;
-            }
-        }
-        N[eid].val = val;
-        N[eid].B = B;
-        N[eid].next = head[num];
-        head[num] = eid++;
-    }
-    int HASH_search(int val)
-    {
-        int num = HASH_num(val);
-        for(int i=head[num];i+1;i=N[i].next){
-            if(N[i].val==val) return N[i].B;
-        }
-        return -1;
-    }
-}
-
-namespace _BSGS
-{
-    using namespace HASH;
-    ll qp(ll base,ll n,ll mod)
-    {
-        ll res = 1;
-        while(n){
-            if(n&1) (res *= base) %= mod;
-            (base *= base) %= mod;
-            n >>= 1;
-        }
-        return res;
-    }
-    int BSGS(int a,int b,int p)//令a^X = b的X = A * ceil(sqrt{p}) - B，A,B \in [0,ceil(sqrt{p})]
-    {
-        a %= p,b %= p;
-        if(a==0){
-            if(b==0) return 0;//0^0 = 1
-            else return -1;
-        }
-        init();
-        int sq = ceil(sqrt(p)+0.5);//保证枚举上界符合要求
-        int tmp = 1,base = qp(a,sq,p);
-        REP(i,0,sq){
-            HASH_insert(1LL * b * tmp % p,i);
-            tmp = 1LL * tmp * a % p;
-        }
-        tmp = base;
-        REP(i,1,sq){//从1开始枚举保证最小
-            int fin = HASH_search(tmp);
-            if(fin != -1) return i * sq - fin;
-            tmp = 1LL * base * tmp % p;
-        }
-        return -1;
-    }
-}
-
-/*{//求x^a \equiv b (%p),p是素数的公式、、用时将其放入BSGS中
-    int generator(int p)
-    {
-        if(p<2) return -1;
-        int phi = p - 1,n = phi;
-        vector<int> fact;
-        for (int i = 2; i * i <= n; ++i){
-            if (n % i == 0){
-                fact.push_back(i);
-                while (n % i == 0) n /= i;
-            }
-        }
-        if (n > 1) fact.push_back(n);
-        for (int res = 1; res <= p; ++res){
-            bool ok = true;
-            for (int factor : fact){
-                if (qp(res, phi / factor, p) == 1){
-                    ok = false;
+        if (p != 2 && p != 4) {
+            if (p % 2 == 0) {
+                p /= 2;
+            }  //此时只有p^e才能有原根
+            for (int i = 2; i * i <= p; i++) {
+                if (p % i == 0) {
+                    while (p % i == 0) {
+                        p /= i;
+                    }
+                    if (i == 2 || p != 1) {
+                        return -1;
+                    }
                     break;
                 }
             }
-            if (ok) return res;
         }
-        return -1;
-    }
+    }  //能过这个无名函数，说明p必有原根
 
-    int formula1(int a,int b,int p)//求x^a \equiv b (%p),p是素数的公式、、用时将其放入BSGS中
+    int phi = p;
     {
-        int g = generator(p);
-        int c = BSGS(qp(g,a,p),b,p);
-        if(c==-1) return -1;
-        return qp(g,c,p);
-    }
-}*/
-
-namespace _Ex_BSGS
-{
-    using namespace HASH;
-    ll qp(ll base,ll n,ll mod)
-    {
-        ll res = 1;
-        while(n){
-            if(n&1) (res *= base) %= mod;
-            (base *= base) %= mod;
-            n >>= 1;
-        }
-        return res;
-    }
-    int BSGS(int a,int b,int p)
-    {
-        init();
-        int sq = ceil(sqrt(p)+0.5);
-        int tmp = 1,base = qp(a,sq,p);
-        REP(i,0,sq){
-            HASH_insert(1LL * b * tmp % p,i);
-            tmp = 1LL * tmp * a % p;
-        }
-        tmp = base;
-        REP(i,1,sq){
-            int fin = HASH_search(tmp);
-            if(fin != -1) return i * sq - fin;
-            tmp = 1LL * base * tmp % p;
-        }
-        return -1;
-    }
-    int Ex_BSGS(int a,int b,int p)
-    {
-        int div,_a = 1,k = 0;
-        while((div = __gcd(a,p)) != 1){
-            if(_a == b) return k;
-            if(b % div != 0) return -1;
-            b /= div;
-            p /= div;
-            _a = 1LL * _a * a / div % p;
-            k++;
-        }
-        int phi = p,tmp = p;
-        for(int i=2;i*i<=tmp;i++){
-            if(tmp%i==0){
+        int tmp = p;
+        for (int i = 2; i * i <= tmp; ++i) {
+            if (tmp % i == 0) {
                 phi = phi / i * (i - 1);
-                while(tmp%i==0) tmp /= i;
+                while (tmp % i == 0) {
+                    tmp /= i;
+                }
             }
         }
-        if(tmp>1) phi = phi / tmp * (tmp - 1);
-        int ans = BSGS(a,1LL*b*qp(_a,phi-1,p)%p,p);
-        return ans == -1 ? -1 : ans + k;
+        if (tmp > 1) {
+            phi = phi / tmp * (tmp - 1);
+        }
+    }  // O(\sqrt{p}) 得到 p 的 phi 值
+
+    vector<int> fact;
+    {
+        int n = phi;
+        for (int i = 2; i * i <= n; ++i) {
+            if (n % i == 0) {
+                fact.push_back(i);
+                while (n % i == 0) {
+                    n /= i;
+                }
+            }
+        }
+        if (n > 1) {
+            fact.push_back(n);
+        }
+    }  //得到 phi 的素因子
+
+    for (int g = 1; g <= p; ++g) {
+        if (__gcd(g, p) != 1) {
+            continue;
+        }  //原根与模数互质
+        bool ok = true;
+        for (int factor : fact) {
+            if (power(g, phi / factor, p) == 1) {
+                ok = false;
+                break;
+            }
+        }
+        if (ok) {
+            return g;
+        }
     }
+    assert(0);
+    return -1;
+}
+/*
+求x^a = b(\bmod p),且p是素数（保证了gcd(a,p)=1且p有原根）的公式。
+假设x = g^c，则(g^c)^a=b(\bmod p) -> (g^a)^c=b(\bmod p)
+使用bsgs求出特解c，则求出x=g^c
+通解是x = g^{ c + k * \frac{\varphi(p)}{gcd(\varphi(p),a)} }
+*/
+i64 formula1(int a, int b, int p) {
+    int g = generator(p);
+    int c = bsgs(power(g, a, p), b, p);
+    if (c == -1) {
+        return -1;
+    }
+    return power(g, c, p);
 }
 
-namespace _Ex_BSGS//求通解版本
-{
-    using namespace HASH;
-    ll qp(ll base,ll n,ll mod)
+/*
+求解 a^x=b(\bmod p)，gcd(a,p)!=1的情况
+通过提取a，来尽量使 p / d'与 a 互质，其中d' = gcd(a^k,p)
+如果合法地得到该式：A*a^{x-o}\equiv \frac{b}{d'}(\bmod \frac{p}{d'}) ，其中A =
+\frac{a}{d'} 当\frac{a}{d'} = \frac{b}{d'}：此时x有唯一解x； 否则有无穷解x = x'
++ k*H，其中x’是上式用bsgs解出的特解，H是bsgs(a,1,\frac{p}{d'})解出的解
+*/
+
+ll ExBsgs(int a, int b, int p) {
+    ll A = 1, o = 0;
     {
-        ll res = 1;
-        while(n){
-            if(n&1) (res *= base) %= mod;
-            (base *= base) %= mod;
-            n >>= 1;
-        }
-        return res;
-    }
-    int BSGS(int a,int b,int p)
-    {
-        init();
-        int sq = ceil(sqrt(p)+0.5);
-        int tmp = 1,base = qp(a,sq,p);
-        REP(i,0,sq){
-            HASH_insert(1LL * b * tmp % p,i);
-            tmp = 1LL * tmp * a % p;
-        }
-        tmp = base;
-        REP(i,1,sq){
-            int fin = HASH_search(tmp);
-            if(fin != -1) return i * sq - fin;
-            tmp = 1LL * base * tmp % p;
-        }
-        return -1;
-    }
-    pair<ll,ll> Ex_BSGS(int a,int b,int p)//返回特解与最小间距、、
-    {
-        int div,_a = 1%p,k = 0;
-        while((div = __gcd(a,p)) != 1){
-            if(_a == b) return mp(k,-1);//唯一解
-            if(b % div != 0) return mp(-1,-1);
+        ll div;
+        while ((div = __gcd(a, p)) != 1) {
+            if (A == b) {
+                return o;
+            }
+            if (b % div != 0) {
+                return -1;
+            }
             b /= div;
             p /= div;
-            _a = 1LL * _a * a / div % p;
-            k++;
+            A = A * a / div % p;
+            o++;
         }
-        int phi = p,tmp = p;
-        for(int i=2;i*i<=tmp;i++){
-            if(tmp%i==0){
+    }
+    int phi = p;
+    {
+        int tmp = p;
+        for (int i = 2; i * i <= tmp; i++) {
+            if (tmp % i == 0) {
                 phi = phi / i * (i - 1);
-                while(tmp%i==0) tmp /= i;
+                while (tmp % i == 0)
+                    tmp /= i;
             }
         }
-        if(tmp>1) phi = phi / tmp * (tmp - 1);
-        int ans = BSGS(a,1LL*b*qp(_a,phi-1,p)%p,p);
-        int H = BSGS(a,1%p,p);
-        return ans == -1 ? mp(-1,-1) : mp(ans+k,H);
+        if (tmp > 1) {
+            phi = phi / tmp * (tmp - 1);
+        }
     }
+    int X = bsgs(a, b * power(A, phi - 1, p) % p, p);
+    // X是特解
+    int H = bsgs(a, 1, p);  // H是最小间距，也即a的阶
+    return X == -1 ? -1 : X + o;
 }
--- 求原根.cpp
+++ 求原根.cpp
@@ -1,65 +1,104 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 typedef long long ll;
+constexpr int mod = 1e9 + 7;
 
-namespace get_G//求一般的原根、、多一步求phi的过程
-{
-    ll qp(ll base,ll n,ll mod)
-    {
-        ll res = 1;
-        while(n){
-            if(n&1) (res *= base) %= mod;
-            (base *= base) %= mod;
-            n >>= 1;
+ll power(ll a, int b, int p = mod) {
+    ll res = 1;
+    for (; b; b /= 2, a *= a, a %= p) {
+        if (b % 2) {
+            res *= a;
+            res %= p;
         }
-        return res;
     }
+    return res;
+}
 
-    int generator(int p)
+/*
+首先，一个正整数 n 有原根的充要条件是 n=2,4,p^e,2p^e，其中，p奇素数，e为正整数。
+当保证有原根时，对于原根g，满足对于所有phi的素因子{p_i}，都有g^{\frac{phi}{p_i}}!=
+1(\mod p) 所以从小到大枚举g，就得出g是多少。常见的原根都很小
+*/
+
+int generator(int p) {
     {
-        if(p<2) return -1;
-        int phi = p,tmp = p;
-        for (int i = 2; i * i <= tmp; ++i){
-            if(tmp % i == 0){
-                phi = phi / i * (i - 1);
-                while(tmp % i == 0) tmp /= i;
-            }
-        }
-        if(tmp > 1) phi = phi / tmp * (tmp - 1);
-        int n = phi;
-        vector<int> fact;
-        for (int i = 2; i * i <= n; ++i){
-            if (n % i == 0){
-                fact.push_back(i);
-                while (n % i == 0) n /= i;
-            }
-        }
-        if (n > 1) fact.push_back(n);
-        for (int res = 1; res <= p; ++res){
-            if(__gcd(res,p) != 1) continue;
-            bool ok = true;
-            for (int factor : fact){
-                if (qp(res, phi / factor, p) == 1){
-                    ok = false;
+        if (p != 2 && p != 4) {
+            if (p % 2 == 0) {
+                p /= 2;
+            }  //此时只有p^e才能有原根
+            for (int i = 2; i * i <= p; i++) {
+                if (p % i == 0) {
+                    while (p % i == 0) {
+                        p /= i;
+                    }
+                    if (i == 2 || p != 1) {
+                        return -1;
+                    }
                     break;
                 }
             }
-            if (ok) return res;
         }
-        return -1;
+    }  //能过这个无名函数，说明p必有原根
+
+    int phi = p;
+    {
+        int tmp = p;
+        for (int i = 2; i * i <= tmp; ++i) {
+            if (tmp % i == 0) {
+                phi = phi / i * (i - 1);
+                while (tmp % i == 0) {
+                    tmp /= i;
+                }
+            }
+        }
+        if (tmp > 1) {
+            phi = phi / tmp * (tmp - 1);
+        }
+    }  // O(\sqrt{p}) 得到 p 的 phi 值
+
+    vector<int> fact;
+    {
+        int n = phi;
+        for (int i = 2; i * i <= n; ++i) {
+            if (n % i == 0) {
+                fact.push_back(i);
+                while (n % i == 0) {
+                    n /= i;
+                }
+            }
+        }
+        if (n > 1) {
+            fact.push_back(n);
+        }
+    }  //得到 phi 的素因子
+
+    for (int g = 1; g <= p; ++g) {
+        if (__gcd(g, p) != 1) {
+            continue;
+        }  //原根与模数互质
+        bool ok = true;
+        for (int factor : fact) {
+            if (power(g, phi / factor, p) == 1) {
+                ok = false;
+                break;
+            }
+        }
+        if (ok) {
+            return g;
+        }
     }
+    assert(0);
+    return -1;
 }
 
-int main()
-{
+int main() {
     int T;
     int P;
-    scanf("%d",&T);
-    while(T--)
-    {
-        scanf("%d",&P);
-        int g = get_G::generator(P);
-        printf("%d\n",g);
+    scanf("%d", &T);
+    while (T--) {
+        scanf("%d", &P);
+        int g = generator(P);
+        printf("%d\n", g);
     }
     return 0;
 }
