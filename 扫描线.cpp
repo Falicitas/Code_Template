d9d49af718cb6e25451fee46b00eb527f38499ea
Kinesis <1019386658@qq.com>
2021-10-06 23:41:52 +0800

    增加了扫描线板子

--- 扫描线.cpp
+++ 扫描线.cpp
@@ -1,48 +1,89 @@
-struct ScanLine
-{
-    double x;
-    double upy,dny;
-    int st;//1 in, -1 out
-    ScanLine(double x0=0,double upy=0,double dny=0,int st=0):upy(upy),dny(dny),st(st){x=x0;}//confrontation to define x(p)
-    bool operator<(const ScanLine&rhs) const
-    {
-        return x < rhs.x;
-    }
-}S[maxn<<1];
+#include <bits/stdc++.h>
+using namespace std;
 
-double Y[maxn];
-struct segtree{
-	int n;
-	static const int N = 1e5 + 5;
-	struct node{int l,r,v;double len;}e[N<<2];
-	#define l(p) e[p].l
-	#define r(p) e[p].r
-	#define v(p) e[p].v
-	#define len(p) e[p].len
-	#define ls p<<1
-	#define rs p<<1|1
-	void up(int p)
-	{
-	    if(v(p)) len(p) = Y[r(p)+1] - Y[l(p)];
-	    else len(p) = len(ls) + len(rs);
-	}
-	void bld(int p,int l,int r){
-		l(p)=l;r(p)=r;
-		if(l==r){v(p) = 0,len(p) = 0;return;}//what decide v(p) = 0 is whether initial value is zero.Another initial function is to link p to l and r.
-		int mid=l+r>>1;
-		bld(ls,l,mid);bld(rs,mid+1,r);
-		up(p);
-	}
-	void init(int _n){n=_n;bld(1,1,n);} //use it, instead of bld.
-	void chg(int p,int cl,int cr,int v){
-	    if(cl<=l(p)&&r(p)<=cr){
-            v(p) += v;
-            up(p);
-            return ;
-	    }
-		int mid=l(p)+r(p)>>1;
-		if(cl<=mid) chg(ls,cl,cr,v);
-		if(cr>mid) chg(rs,cl,cr,v);
-		up(p);
-	}
-}seg;
+/*使用标记永久化，此时sum[1]记录线段覆盖的区间长度*/
+struct SegmentTree {
+    int n;
+    std::vector<int> tag, sum;
+    std::vector<int> y;  //线段树点的实际y值
+    SegmentTree(int n, vector<int> y_) : n(n), tag(4 * n), sum(4 * n), y(y_) {}
+    void rangeAdd(int p, int l, int r, int x, int y, int v) {
+        if (r <= x || y <= l) {
+            return;
+        }
+        if (x <= l && r <= y) {
+            add(p, l, r, v);
+            return;
+        }
+        int m = (l + r) / 2;
+        rangeAdd(2 * p, l, m, x, y, v);
+        rangeAdd(2 * p + 1, m, r, x, y, v);
+        pull(p, l, r);
+    }
+    void add(int p, int l, int r, int v) {
+        tag[p] += v;
+        pull(p, l, r);
+    }
+    void pull(int p, int l, int r) {
+        if (tag[p] > 0) {
+            sum[p] = y[r] - y[l];
+        } else if (r - l == 1) {
+            sum[p] = 0;
+        } else {
+            sum[p] = sum[2 * p] + sum[2 * p + 1];
+        }
+    }
+    void rangeAdd(int l, int r, int v) { rangeAdd(1, 0, n, l, r, v); }
+    int find(int p,
+             int l,
+             int r) {  //如果sum[1]!=n，即不覆盖所有区间，则找一个空闲的区间
+        if (r - l == 1) {
+            return l;
+        }
+        int m = (l + r) / 2;
+        if (sum[2 * p] < m - l) {
+            return find(2 * p, l, m);
+        } else {
+            return find(2 * p + 1, m, r);
+        }
+    }
+    int find() {
+        if (sum[1] == n) {
+            return -1;
+        }
+        return find(1, 0, n);
+    }
+};
+
+/*求n个矩形的面积并*/
+int main() {
+    int n;
+    cin >> n;
+    // y1,y2,xi,1/-1.y离散。扫描线垂直x轴，向x正半轴移动
+    using t4 = tuple<int, int, int, int>;
+    vector<t4> line;
+    vector<int> by;
+    for (int i = 0; i < n; i++) {
+        int x1, y1, x2, y2;
+        cin >> x1 >> y1 >> x2 >> y2;
+        line.emplace_back(x1, 1, y1, y2);
+        line.emplace_back(x2, -1, y1, y2);
+        by.emplace_back(y1);
+        by.emplace_back(y2);
+    }
+    sort(line.begin(), line.end());
+    sort(by.begin(), by.end());  //离散的y，插入线段树中
+    auto border = unique(by.begin(), by.end());
+
+    SegmentTree tr(2 * n, vector<int>(by.begin(), border));
+
+    using i64 = long long;
+    i64 sum = 0;
+    for (int i = 0; i < 2 * n - 1; i++) {
+        int t1 = lower_bound(by.begin(), border, get<2>(line[i])) - by.begin();
+        int t2 = lower_bound(by.begin(), border, get<3>(line[i])) - by.begin();
+        tr.rangeAdd(t1, t2, get<1>(line[i]));
+        sum += 1ll * tr.sum[1] * (get<0>(line[i + 1]) - get<0>(line[i]));
+    }
+    cout << sum << "\n";
+}
