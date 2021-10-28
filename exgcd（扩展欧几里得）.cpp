c2932e5a7b0a425c9729ed1cf1cfd6a975fdf283
Kinesis <1019386658@qq.com>
2021-10-28 13:03:37 +0800

    更新部分木板

--- exgcd（扩展欧几里得）.cpp
+++ exgcd（扩展欧几里得）.cpp
@@ -1,25 +1,27 @@
-namespace Exgcd
-{
-    ll ex_gcd(ll a, ll b, ll& x, ll& y)
-    {
-        if (b == 0) {
-            x = 1;
-            y = 0;
-            return a;
-        }
-        ll d = ex_gcd(b, a % b, x, y);
-        ll temp = x;
-        x = y;
-        y = temp - a / b * y;
-        return d;
+#include <bits/stdc++.h>
+using namespace std;
+using i64 = long long;
+
+i64 exgcd(i64 a, i64 b, i64& x, i64& y) {
+    if (b == 0) {
+        x = 1;
+        y = 0;
+        return a;
     }
-    bool liEu(ll a, ll b, ll c, ll& x, ll& y)
-    {
-        ll d = ex_gcd(a, b, x, y);
-        if (c % d != 0) return 0;
-        ll k = c / d;
-        x *= k;//得到一组特解，求最小非负整数，则x = (x % (b / d) + b / d) % (b / d);
-        y *= k;
-        return 1;
-    }
+    i64 d = exgcd(b, a % b, x, y);
+    i64 temp = x;
+    x = y;
+    y = temp - a / b * y;
+    return d;
+}
+
+bool liEu(i64 a, i64 b, i64 c, i64& x, i64& y) {
+    i64 d = exgcd(a, b, x, y);
+    if (c % d != 0)
+        return 0;
+    i64 k = c / d;
+    x *= k;  //得到一组特解，求最小非负整数，则x = (x % (b / d) + b / d) % (b /
+             // d);
+    y *= k;
+    return 1;
 }
--- 线段树.cpp
+++ 线段树.cpp
@@ -3,14 +3,15 @@ using namespace std;
 using i64 = long long;
 template <class Info,
           class Merge = std::plus<Info>>
-struct SegmentTree {
+struct SegmentTree {  //下面有[l,r]版本
     const int n;
     const Merge merge;
     std::vector<Info> info;
     SegmentTree(int n)  //初始化数组
         : n(n), merge(Merge()), info(4 << std::__lg(n)) {}
     SegmentTree(std::vector<Info> init)
-        : SegmentTree(init.size()) {  //原来构造函数能调用构造函数。O(n)构造线段树
+        : SegmentTree(
+              init.size()) {  //原来构造函数能调用构造函数。O(n)构造线段树
         std::function<void(int, int, int)> build = [&](int p, int l, int r) {
             if (r - l == 1) {
                 info[p] = init[l];
@@ -23,9 +24,7 @@ struct SegmentTree {
         };
         build(1, 0, n);
     }
-    void pull(int p) {
-        info[p] = merge(info[2 * p], info[2 * p + 1]);
-    }
+    void pull(int p) { info[p] = merge(info[2 * p], info[2 * p + 1]); }
     void modify(int p, int l, int r, int x, const Info& v) {
         if (r - l == 1) {
             info[p] = v;
@@ -50,7 +49,8 @@ struct SegmentTree {
             return info[p];
         }
         int m = (l + r) / 2;
-        return merge(rangeQuery(2 * p, l, m, x, y), rangeQuery(2 * p + 1, m, r, x, y));
+        return merge(rangeQuery(2 * p, l, m, x, y),
+                     rangeQuery(2 * p + 1, m, r, x, y));
     }
     Info rangeQuery(int l, int r) {  //区间查询[l,r)
         return rangeQuery(1, 0, n, l, r);
@@ -58,27 +58,105 @@ struct SegmentTree {
 };
 
 struct Info {
-    // int l;//示例：这里Info维护区间中连续子段的个数，s维护该段连续的子段的个数。x=(i不是末尾 && a[i]<=a[i+1])
-    // int r;
-    // int len;
-    // i64 s;
-    // Info()
+    // int
+    // l;//示例：这里Info维护区间中连续子段的个数，s维护该段连续的子段的个数。x=(i不是末尾
+    // && a[i]<=a[i+1]) int r; int len; i64 s; Info()
     //     : l(0), r(0), len(0), s(0) {}
     // Info(int x)
     //     : l(x), r(x), len(1), s(x + 1) {}
     /*
-    这里说下 s(x+1) 的问题。由于最长连续段 [x,y) 的尾的 x=0 ，故初始化先统计 [x,y),[x+1,y),[x+2,y) 的子段
-    （包括 [x,x+1),[x+1,x+2),... ，故 s = x+1）
-    对于查询 [l,r] 连续子段的个数，仅查 [l,r) 即可，最后少了个 [r,r+1) ，所以结果为 rangeQuery(l,r) + 1。
-    还有注意下标从 0 开始。
+    这里说下 s(x+1) 的问题。由于最长连续段 [x,y) 的尾的 x=0 ，故初始化先统计
+    [x,y),[x+1,y),[x+2,y) 的子段 （包括 [x,x+1),[x+1,x+2),... ，故 s = x+1）
+    对于查询 [l,r] 连续子段的个数，仅查 [l,r) 即可，最后少了个 [r,r+1)
+    ，所以结果为 rangeQuery(l,r) + 1。 还有注意下标从 0 开始。
     */
 };
 
 Info operator+(const Info& a, const Info& b) {
     // Info c;
-    // c.l = a.l == a.len ? a.len + b.l : a.l;//示例：证明返回Info(),l,r维护正常：Info()时，l = len = 0，所以会触发a.len+b.l
-    // c.r = b.r == b.len ? a.r + b.len : b.r;
-    // c.len = a.len + b.len;
-    // c.s = a.s + b.s + 1LL * a.r * b.l;
-    // return c;
+    // c.l = a.l == a.len ? a.len + b.l :
+    // a.l;//示例：证明返回Info(),l,r维护正常：Info()时，l = len =
+    // 0，所以会触发a.len+b.l c.r = b.r == b.len ? a.r + b.len : b.r; c.len =
+    // a.len + b.len; c.s = a.s + b.s + 1LL * a.r * b.l; return c;
 }
+
+namespace X {
+
+template <class Info,
+          class Merge = std::plus<Info>>
+struct SegmentTree {  //下面有
+    const int n;
+    const Merge merge;
+    std::vector<Info> info;
+    SegmentTree(int n_)  //初始化数组
+        : n(n_ - 1), merge(Merge()), info(4 << std::__lg(n)) {}
+    SegmentTree(std::vector<Info> init)
+        : SegmentTree(
+              init.size()) {  //原来构造函数能调用构造函数。O(n)构造线段树
+        std::function<void(int, int, int)> build = [&](int p, int l, int r) {
+            if (r == l) {
+                info[p] = init[l];
+                return;
+            }
+            int m = (l + r) / 2;
+            build(2 * p, l, m);
+            build(2 * p + 1, m + 1, r);
+            pull(p);
+        };
+        build(1, 1, n);
+    }
+    void pull(int p) { info[p] = merge(info[2 * p], info[2 * p + 1]); }
+    void modify(int p, int l, int r, int x, const Info& v) {
+        if (r == l) {
+            info[p] = v;
+            return;
+        }
+        int m = (l + r) / 2;
+        if (x <= m) {
+            modify(2 * p, l, m, x, v);
+        } else {
+            modify(2 * p + 1, m + 1, r, x, v);
+        }
+        pull(p);
+    }
+    void modify(int p, const Info& v) { modify(1, 1, n, p, v); }
+    Info rangeQuery(int p, int l, int r, int x, int y) {
+        if (x <= l && r <= y) {
+            return info[p];
+        }
+        int m = (l + r) / 2;
+        if (y <= m) {
+            return rangeQuery(2 * p, l, m, x, y);
+        } else if (x > m) {
+            return rangeQuery(2 * p + 1, m + 1, r, x, y);
+        } else {
+            return merge(rangeQuery(2 * p, l, m, x, y),
+                         rangeQuery(2 * p + 1, m + 1, r, x, y));
+        }
+    }
+    Info rangeQuery(int l, int r) { return rangeQuery(1, 1, n, l, r); }
+};
+
+struct Info {
+    // int
+    // l;//示例：这里Info维护区间中连续子段的个数，s维护该段连续的子段的个数。x=(i不是末尾
+    // && a[i]<=a[i+1]) int r; int len; i64 s; Info()
+    //     : l(0), r(0), len(0), s(0) {}
+    // Info(int x)
+    //     : l(x), r(x), len(1), s(x + 1) {}
+    /*
+    这里说下 s(x+1) 的问题。由于最长连续段 [x,y) 的尾的 x=0 ，故初始化先统计
+    [x,y),[x+1,y),[x+2,y) 的子段 （包括 [x,x+1),[x+1,x+2),... ，故 s = x+1）
+    对于查询 [l,r] 连续子段的个数，仅查 [l,r) 即可，最后少了个 [r,r+1)
+    ，所以结果为 rangeQuery(l,r) + 1。 还有注意下标从 0 开始。
+    */
+};
+
+Info operator+(const Info& a, const Info& b) {
+    // Info c;
+    // c.l = a.l == a.len ? a.len + b.l :
+    // a.l;//示例：证明返回Info(),l,r维护正常：Info()时，l = len =
+    // 0，所以会触发a.len+b.l c.r = b.r == b.len ? a.r + b.len : b.r; c.len =
+    // a.len + b.len; c.s = a.s + b.s + 1LL * a.r * b.l; return c;
+}
+}  // namespace X
